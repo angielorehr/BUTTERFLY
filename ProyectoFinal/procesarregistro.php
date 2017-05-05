be8a775e35e959baea333952ade1bc53f1fea3af
Estudiantes <Estudiantes@labing50213.udistrital.edu.co>
2017-05-05 16:48:20 -0500

    ingreso de datos a DB

--- ProyectoFinal/procesarregistro.php
+++ ProyectoFinal/procesarregistro.php
@@ -1,10 +1,31 @@
 <?php
 
-$enlace= mysqli_connect('localhost', 'root', 'mysql2017', 'catastral', 3306);
-$cadena="INSERT INTO `catastral`.`estudiante`
-(`nombre`,
-`edad`,
-`codigo-estudiante`)
+$enlace= mysqli_connect('localhost', 'root', 'mysql2017', 'formulario', 3306);
+$cadena="INSERT INTO `formulario`.`registro`
+(`nombreMascota`,
+`edadMascota`,
+`nombrePropietario`,
+`telefonoFijo`,
+`telefonoCelular`,
+`direccion`,
+`email`,
+`numero de identificacion`,
+`estrato`,
+`clave`,
+`confirmacion`)
 VALUES
-('".$_REQUEST['nombre']."',".$_REQUEST['edad'].",".$_REQUEST['codigoestudiante'].")";
+(<{nombreMascota: }>,
+<{edadMascota: }>,
+<{nombrePropietario: }>,
+<{telefonoFijo: }>,
+<{telefonoCelular: }>,
+<{direccion: }>,
+<{email: }>,
+<{numeroDeIdentificacion: }>,
+<{estrato: }>,
+<{clave: }>,
+<{confirmacion: }>);
+
+VALUES
+('".$_REQUEST['nombreMascota']."','".$_REQUEST['edadMascota']."','".$_REQUEST['nombrePropietario']."','".$_REQUEST['telefonoFijo']."','".$_REQUEST['telefonoCelular']."','".$_REQUEST['direccion']."','".$_REQUEST['email']."','".$_REQUEST['NumeroDeIdentificacion']."','".$_REQUEST['estrato']."',".$_REQUEST['clave'].",".$_REQUEST['confirmacion'].")";
 mysqli_query($enlace, $cadena);
